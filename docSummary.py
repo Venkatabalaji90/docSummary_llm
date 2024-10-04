43a7a5e0fb223d6fd5121b828889422b48a371c6
Venkata Balaji <35142967+Venkatabalaji90@users.noreply.github.com>
2024-10-04 18:35:27 +0530

    passing message to context

--- docSummary.py
+++ docSummary.py
@@ -39,7 +39,7 @@ def get_response(client_,vectorstore_,message_):
     conversation = [
     {
         "role": "user",
-        "content": [{"text": retrieved_text}],
+        "content": [{"text": message_+retrieved_text}],
     }
     ]
 
